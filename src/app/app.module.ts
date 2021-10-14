496b9f2d29f96533b8397626cc136acc8941db61
Haim Guel Quiroz <hgq_2000@hotmail.com>
2021-10-14 11:13:55 -0500

    Add header and recipies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent
   ],
   imports: [
     BrowserModule
