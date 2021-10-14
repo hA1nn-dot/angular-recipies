88633dff27b1947cff2f98628ab1d935bb59c8e5
Haim Guel Quiroz <hgq_2000@hotmail.com>
2021-10-14 11:15:18 -0500

    Add recipies list in recipies folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipiesListComponent } from './recipies/recipies-list/recipies-list.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipiesComponent
+    RecipiesComponent,
+    RecipiesListComponent
   ],
   imports: [
     BrowserModule
