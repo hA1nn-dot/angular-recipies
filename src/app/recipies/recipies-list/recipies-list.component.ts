693d429b01248a340d3b191f995e2e4accabbcbb
Haim Guel Quiroz <hgq_2000@hotmail.com>
2021-12-14 23:14:39 -0600

    Create a recipe-model.ts to show recipes

--- src/app/recipies/recipies-list/recipies-list.component.ts
+++ src/app/recipies/recipies-list/recipies-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipie.model';
 
 @Component({
   selector: 'app-recipies-list',
   templateUrl: './recipies-list.component.html',
-  styleUrls: ['./recipies-list.component.css']
+  styleUrls: ['./recipies-list.component.css'],
 })
 export class RecipiesListComponent implements OnInit {
+  recipies: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'Description',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'Description',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
